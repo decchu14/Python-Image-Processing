4502b1225102e309f5090c056fd0e58034a7da31
decchu14 <1decchu14@gmail.com>
2020-09-16 05:48:57 +0530

    modified comments

--- 2_ImageProcessing.py
+++ 2_ImageProcessing.py
@@ -1,19 +1,20 @@
 # this simple code is to filter or convert the image
 from PIL import Image, ImageFilter
 img = Image.open('pokedex/pikachu.jpg')
-0
+
 # will filter the image to blur or smooth or sharpen and store it in variable named filtered_img
 filtered_image = img.filter(ImageFilter.BLUR)  # SMOOTH, SHARPEN
 
 # will save the filtered_img in new file named blur.png as png format
-# img can be found inside pokedex folder
+# img can be found inside processed images folder
 filtered_image.save("blur.png", "png")
 
-# converts the img to black and white and stre it in variable named fil_img
+# converts the img to black and white and store it in variable named fil_img
 fil_img = img.convert('L')
 
 # will save the fil_img in new file named grey.png as png format
-fil_img.save("grey.png", "png")  # imgr can be found inside pokedex folder
+# img can be found inside processed images folder
+fil_img.save("grey.png", "png")
 
 # rotates the image in 180 degree direction and saves it in grey.png file as png format
 crooked = img.rotate(180)
